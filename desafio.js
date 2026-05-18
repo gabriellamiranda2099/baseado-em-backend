ea80d1758d83419c9f65e71dce3e8aae32071044
gabriellamiranda2099 <gabriella.miranda.leite@escola.pr.gov.br>
2026-05-18 11:50:33 +0000

    versao1

--- desafio.js
+++ desafio.js
@@ -1,7 +1,19 @@
-const produto= vassoura;
-const categoria= limpeza; 
-const loja= supermercado;
-let preco= 20.00;
-let desconto= 0.10; 
-let precoFinal= preco - (preco * desconto);
-console.log(''
+    const loja = "SuperMercado";
+const produto = "Vassoura";
+const categoria = "Limpeza";
+let preco = 20.00;
+let desconto = 0.10;
+let precoFinal = preco - (preco * desconto);
+
+let desconto2 = 0.20;
+let precoFinal2 = preco - (preco * desconto2);
+
+let compras = 500;
+let final = compras - (compras * desconto2);
+
+let economia = (compras - final)
+
+console.log(`Venha conferir nosso incrível produto: ${produto}, que foi de ${preco} para ${precoFinal}! É a melhor ferramenta para sua ${categoria}.`)
+console.log(`${desconto} * ${preco} = ${precoFinal}`)
+
+console.log(`A partir de R$500,00, você tem 20% de desconto! Ao final, você vai pagar: R$${final}, economizando R$${economia}`)
